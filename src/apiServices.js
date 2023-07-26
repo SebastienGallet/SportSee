5977315beb035ae6aefd7011ac30571b3f4bf5ae
Sebastien Gallet <sgallet.dev@gmail.com>
2023-07-26 09:49:21 +0200

    api

--- src/apiServices.js
+++ src/apiServices.js
@@ -1,43 +1,87 @@
+import mockedData from './data/mockeddata.json';
 import axios from 'axios';
+
+const USE_MOCKED_DATA = false;
 
 const baseURL = 'http://localhost:3000';
 
 export const getUserInfo = async (userId) => {
-  try {
-    const response = await axios.get(`${baseURL}/user/${userId}`);
-    return response.data;
-  } catch (error) {
-    console.error('Erreur lors de la récupération des informations de l\'utilisateur:', error);
-    return null;
+  if (USE_MOCKED_DATA) {
+    try {
+      const response = mockedData.USER_MAIN_DATA.find((user) => user.id === userId);
+      return response;
+    } catch (error) {
+      console.error('Erreur lors de la récupération des informations de l\'utilisateur :', error);
+      return null;
+    }
+  } else {
+    try {
+      const response = await axios.get(`${baseURL}/user/${userId}`);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Erreur lors de la récupération des informations de l\'utilisateur :', error);
+      return null;
+    }
   }
 };
 
 export const getUserActivity = async (userId) => {
-  try {
-    const response = await axios.get(`${baseURL}/user/${userId}/activity`);
-    return response.data;
-  } catch (error) {
-    console.error('Erreur lors de la récupération de l\'activité de l\'utilisateur:', error);
-    return null;
+  if (USE_MOCKED_DATA) {
+    try {
+      const response = mockedData.USER_ACTIVITY.find((user) => user.userId === userId);
+      return response;
+    } catch (error) {
+      console.error('Erreur lors de la récupération de l\'activité de l\'utilisateur :', error);
+      return null;
+    }
+  } else {
+    try {
+      const response = await axios.get(`${baseURL}/user/${userId}/activity`);
+      return response.data;
+    } catch (error) {
+      console.error('Erreur lors de la récupération de l\'activité de l\'utilisateur :', error);
+      return null;
+    }
   }
 };
 
 export const getUserAverageSessions = async (userId) => {
-  try {
-    const response = await axios.get(`${baseURL}/user/${userId}/average-sessions`);
-    return response.data;
-  } catch (error) {
-    console.error('Erreur lors de la récupération des sessions moyennes de l\'utilisateur:', error);
-    return null;
+  if (USE_MOCKED_DATA) {
+    try {
+      const response = mockedData.USER_AVERAGE_SESSIONS.find((user) => user.userId === userId);
+      return response;
+    } catch (error) {
+      console.error('Erreur lors de la récupération des sessions moyennes de l\'utilisateur :', error);
+      return null;
+    }
+  } else {
+    try {
+      const response = await axios.get(`${baseURL}/user/${userId}/average-sessions`);
+      return response.data;
+    } catch (error) {
+      console.error('Erreur lors de la récupération des sessions moyennes de l\'utilisateur :', error);
+      return null;
+    }
   }
 };
 
 export const getUserPerformance = async (userId) => {
-  try {
-    const response = await axios.get(`${baseURL}/user/${userId}/performance`);
-    return response.data;
-  } catch (error) {
-    console.error('Erreur lors de la récupération de la performance de l\'utilisateur:', error);
-    return null;
+  if (USE_MOCKED_DATA) {
+    try {
+      const response = mockedData.USER_PERFORMANCE.find((user) => user.userId === userId);
+      return response;
+    } catch (error) {
+      console.error('Erreur lors de la récupération de la performance de l\'utilisateur :', error);
+      return null;
+    }
+  } else {
+    try {
+      const response = await axios.get(`${baseURL}/user/${userId}/performance`);
+      return response.data;
+    } catch (error) {
+      console.error('Erreur lors de la récupération de la performance de l\'utilisateur :', error);
+      return null;
+    }
   }
 };
